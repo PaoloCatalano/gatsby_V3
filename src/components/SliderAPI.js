384fd81a0740496243dae6d9035000a8b1a25d33
Paolo Catalano <paolo12587@gmail.com>
2021-04-21 18:12:19 +0200

    no grid center

--- src/components/SliderAPI.js
+++ src/components/SliderAPI.js
@@ -35,53 +35,51 @@ const SliderAPI = () => {
   })
 
   return (
-    <div style={{ display: "grid", placeItems: "center" }}>
-      <Carousel
-        centerMode
-        centerSlidePercentage={100}
-        //   dynamicHeight
-        emulateTouch
-        infiniteLoop
-        // autoPlay
-        interval={6000}
-        renderThumbs={() => {
-          return preview
-        }}
-        // .carousel-root{width: 100% !important;}
-      >
-        {recipes.map((recipe, index) => {
-          const { title, image } = recipe
-          return (
-            <div key={index}>
-              <GatsbyImage
-                image={getImage(image)}
-                className="recipe-img "
-                style={{ height: "70vh" }}
-                alt={title}
-              />
-              <div
-                className="legend opacity"
-                style={{
-                  marginLeft: "-50%",
-                  //   height: "97.9%",
-                  height: "100%",
-                  width: "100%",
-                  fontSize: "2rem",
-                  background: "rgba(0, 0, 0, 0.3)",
-                  top: 0,
-                  display: "grid",
-                  placeItems: "center",
-                  borderRadius: 5,
-                  //   maxWidth
-                }}
-              >
-                {title}
-              </div>
+    <Carousel
+      centerMode
+      centerSlidePercentage={100}
+      //   dynamicHeight
+      emulateTouch
+      infiniteLoop
+      // autoPlay
+      interval={6000}
+      renderThumbs={() => {
+        return preview
+      }}
+      // .carousel-root{width: 100% !important;}
+    >
+      {recipes.map((recipe, index) => {
+        const { title, image } = recipe
+        return (
+          <div key={index}>
+            <GatsbyImage
+              image={getImage(image)}
+              className="recipe-img "
+              style={{ height: "70vh" }}
+              alt={title}
+            />
+            <div
+              className="legend opacity"
+              style={{
+                marginLeft: "-50%",
+                //   height: "97.9%",
+                height: "100%",
+                width: "100%",
+                fontSize: "2rem",
+                background: "rgba(0, 0, 0, 0.3)",
+                top: 0,
+                display: "grid",
+                placeItems: "center",
+                borderRadius: 5,
+                //   maxWidth
+              }}
+            >
+              {title}
             </div>
-          )
-        })}
-      </Carousel>
-    </div>
+          </div>
+        )
+      })}
+    </Carousel>
   )
 }
 
